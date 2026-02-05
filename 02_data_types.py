7966ab53413928f06ea501ab1dc1a34ac2f2394a
abhik soni <asoni88899@gmail.com>
2026-02-05 10:38:22 +0530

    variables

---
+++ 02_data_types.py
@@ -0,0 +1,27 @@
+#python supports built-in data type:
+#       Integer (int): whole numbers(e.g.,10,-5).
+#       Floats (float): decimal number (e.g.,3.24, -0.02)'
+#       Strings (str): text data enclosed in quotes (e.g., "hello", 'python').
+#       booleans (bool):represent True or False.
+#       Lists: ordered, mutable collection (e.g.,[1,2,3]).
+#       tuples : ordered, immutable of uniqe elements (e.g.,(1,2,3)).
+#       sets : unordered collection of uniqe elements(e.g.,{1,2,3}).
+#       dictionaries: key-values pairs (e.g., {"name": "alice" "age":25}).
+
+age = 3
+print(age)
+print(type(age))
+
+cgpa = 8.2
+print(cgpa)
+print(type(cgpa))
+
+name = "harry"
+print(name)
+print(type(name))
+
+
+is_completed = True #can also be false
+print(is_completed)
+print(type(is_completed))
+ 
