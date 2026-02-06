35a7ceed125c97ca6d2b446f1f5470867f3fc781
abhik soni <asoni88899@gmail.com>
2026-02-06 12:19:46 +0530

    04_TYPECASTING

---
+++ 04_TYPECASTING/02_examples.py
@@ -0,0 +1,23 @@
+#covert string to integer
+num_str = "10"
+num_int = int(num_str)
+print(num_int)
+print(type(num_int))
+
+#convert integer to string 
+num = 25
+num_str = str(num)
+print(num_str)
+print(type(num_str))
+
+#convert float to integer
+pi = 3.14
+pi_int= int(pi)
+print(pi_int)
+print(type(pi_int))
+
+#convert integer to string 
+a = 22
+b = str(a)
+print(b)
+print(type(b))
