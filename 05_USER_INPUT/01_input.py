ceb1c390664469a75a34c0fd1755ba02d2db8d1d
abhik soni <asoni88899@gmail.com>
2026-02-07 14:15:32 +0530

    User input

---
+++ 05_USER_INPUT/01_input.py
@@ -0,0 +1,9 @@
+a = input("Enter a nuber")
+print(a)
+
+b = input("Enter a second number: ")
+print(b)
+
+c = input("Enter a third number")
+print(c)
+
---
+++ 05_USER_INPUT/02_add.py
@@ -0,0 +1,10 @@
+# a = input("Enter first number")
+# a = int(a)
+# print(a+3)
+
+a = int(input("Enter first mumber"))
+#a = int(a)(comment because be can also use coverter int into brfore input just above thid message)
+b = int(input("Enter a second number"))
+#b = int(b)(comment because be can also use coverter int into brfore input just above thid message)
+
+print(a+b) 
