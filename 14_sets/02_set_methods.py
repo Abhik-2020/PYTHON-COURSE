17c4146ce331e236f4d2c15bb4599e1a9107452b
abhik soni <asoni88899@gmail.com>
2026-03-12 11:34:21 +0530

    sets

---
+++ 14_sets/02_set_methods.py
@@ -0,0 +1,12 @@
+s = {34, 23, 1, 3, 22}
+
+print(s)
+
+s.add(32)  # invaild if you write this :- print(s.add(32))
+print(s)
+
+s.add(435)
+s.remove(1)
+#s.remove(405637869) #throws an error
+s.discard(797973)
+print(s)
